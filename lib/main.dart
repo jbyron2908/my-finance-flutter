2e662203a8fa742ef506792a69eb0899871a3068
Jonathan Lindoso <jonathan.lindoso@volvocars.com>
2019-05-12 19:33:59 +0200

    File structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_finance_flutter/generated/i18n.dart';
+import 'package:my_finance_flutter/ui/page/home/index.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -20,60 +22,7 @@ class MyApp extends StatelessWidget {
           },
           supportedLocales: i18n.supportedLocales,
       localeResolutionCallback: i18n.resolution(fallback: new Locale("en", "US")),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    I18n i18n = I18n.of(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Text(
-              i18n.greetTo("Flutter")
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
