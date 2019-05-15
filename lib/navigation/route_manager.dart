563eb2cc8fcc3eabab2d5375039df45c9b1d1f99
Jonathan Lindoso <jonathan.lindoso@volvocars.com>
2019-05-15 20:47:23 +0200

    Config routes

---
+++ lib/navigation/route_manager.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:my_finance_flutter/ui/page/home/page.dart';
+import 'package:my_finance_flutter/ui/page/splash/page.dart';
+
+class RouteManager {
+  static const _splashPath = "/splash";
+  static const _homePath = "/";
+
+  static Route configureRoutes(RouteSettings routeSettings) {
+    switch(routeSettings.name) {
+      case _splashPath: return MaterialPageRoute(builder: (context) => SplashPage(), settings: routeSettings);
+      case _homePath: return MaterialPageRoute(builder: (context) => HomePage(), settings: routeSettings);
+      default: return MaterialPageRoute(builder: (context) => HomePage(), settings: routeSettings);
+    }
+  }
+
+  static void navigateToSplash(BuildContext context) {
+    Navigator.pushNamed(context, _splashPath);
+  }
+
+  static void navigateToHome(BuildContext context) {
+    Navigator.pushNamed(context, _homePath);
+  }
+}
