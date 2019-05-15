5d26a3cf241167b7ce657da11690d0fc2b7ab769
Jonathan Lindoso <jonathan.lindoso@volvocars.com>
2019-05-15 16:46:34 +0200

    Extract App widget

--- lib/main_qa.dart
+++ lib/main_qa.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:my_finance_flutter/app.dart';
 import 'package:my_finance_flutter/config/flavor/flavor.dart';
-import 'package:my_finance_flutter/generated/i18n.dart';
-import 'package:my_finance_flutter/ui/page/home/index.dart';
 
 void main() {
   FlavorConfig(
@@ -12,27 +10,5 @@ void main() {
           baseUrl:
               "https://raw.githubusercontent.com/JHBitencourt/ready_to_go/master/lib/json/person_qa.json"));
 
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final i18n = S.delegate;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      localizationsDelegates: {
-        GlobalMaterialLocalizations.delegate,
-        i18n,
-      },
-      supportedLocales: i18n.supportedLocales,
-      localeResolutionCallback:
-          i18n.resolution(fallback: new Locale("en", "US")),
-      home: HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(App());
 }
