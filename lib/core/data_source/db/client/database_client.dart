a74522f90e39fcb84e678442b95aa01ba3bd7546
Jonathan Lindoso <jonathan.lindoso@volvocars.com>
2019-10-15 22:40:56 +0200

    Adding tag to database

--- lib/core/data_source/db/client/database_client.dart
+++ lib/core/data_source/db/client/database_client.dart
@@ -3,16 +3,18 @@ import 'package:my_finance_flutter/core/data_source/db/dao/account_dao.dart';
 import 'package:my_finance_flutter/core/data_source/db/dao/category_dao.dart';
 import 'package:my_finance_flutter/core/data_source/db/dao/operation_dao.dart';
 import 'package:my_finance_flutter/core/data_source/db/dao/payee_dao.dart';
+import 'package:my_finance_flutter/core/data_source/db/dao/tag_dao.dart';
 import 'package:my_finance_flutter/core/data_source/db/table/account_table.dart';
 import 'package:my_finance_flutter/core/data_source/db/table/category_table.dart';
 import 'package:my_finance_flutter/core/data_source/db/table/operation_table.dart';
 import 'package:my_finance_flutter/core/data_source/db/table/payee_table.dart';
+import 'package:my_finance_flutter/core/data_source/db/table/tag_table.dart';
 
 part 'database_client.g.dart';
 
 @UseMoor(
-  tables: [CategoryTable, AccountTable, OperationTable, PayeeTable],
-  daos: [CategoryDao, AccountDao, OperationDao, PayeeDao],
+  tables: [CategoryTable, AccountTable, OperationTable, PayeeTable, TagTable],
+  daos: [CategoryDao, AccountDao, OperationDao, PayeeDao, TagDao],
 )
 class DatabaseClient extends _$DatabaseClient {
   DatabaseClient()
