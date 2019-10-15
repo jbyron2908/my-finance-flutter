5f7a27e0b3d90b55f80dd4826e1e3dd5e78e0afd
Jonathan Lindoso <jonathan.lindoso@volvocars.com>
2019-10-15 21:52:01 +0200

    Adding payee to database

--- lib/core/data_source/db/client/database_client.dart
+++ lib/core/data_source/db/client/database_client.dart
@@ -2,15 +2,17 @@ import 'package:moor_flutter/moor_flutter.dart';
 import 'package:my_finance_flutter/core/data_source/db/dao/account_dao.dart';
 import 'package:my_finance_flutter/core/data_source/db/dao/category_dao.dart';
 import 'package:my_finance_flutter/core/data_source/db/dao/operation_dao.dart';
+import 'package:my_finance_flutter/core/data_source/db/dao/payee_dao.dart';
 import 'package:my_finance_flutter/core/data_source/db/table/account_table.dart';
 import 'package:my_finance_flutter/core/data_source/db/table/category_table.dart';
 import 'package:my_finance_flutter/core/data_source/db/table/operation_table.dart';
+import 'package:my_finance_flutter/core/data_source/db/table/payee_table.dart';
 
 part 'database_client.g.dart';
 
 @UseMoor(
-  tables: [CategoryTable, AccountTable, OperationTable],
-  daos: [CategoryDao, AccountDao, OperationDao],
+  tables: [CategoryTable, AccountTable, OperationTable, PayeeTable],
+  daos: [CategoryDao, AccountDao, OperationDao, PayeeDao],
 )
 class DatabaseClient extends _$DatabaseClient {
   DatabaseClient()
