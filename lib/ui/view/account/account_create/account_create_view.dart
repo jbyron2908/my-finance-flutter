7ba284c8be0b260e7a4309c24efc02223212f36a
Jonathan Lindoso <jonathan.lindoso@volvocars.com>
2019-10-24 13:07:55 +0200

    Undo account create refactor

--- lib/ui/view/account/account_create/account_create_view.dart
+++ lib/ui/view/account/account_create/account_create_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_finance_flutter/core/provider/model/account_model.dart';
 import 'package:my_finance_flutter/core/provider/repository/account/account_repository.dart';
-import 'package:my_finance_flutter/ui/view/account/account_create/widgets/account_create_form.dart';
+import 'package:my_finance_flutter/ui/view/account/account_create/account_create_form.dart';
 
 class AccountCreateView extends StatelessWidget {
   AccountCreateView({Key key}) : super(key: key);
@@ -14,10 +14,12 @@ class AccountCreateView extends StatelessWidget {
       appBar: AppBar(
         title: Text("Create Account"),
       ),
-      body: AccountCreateForm(onSubmit: (AccountModel account) async {
-        await accountRepository.save(account);
-        Navigator.of(context).pop();
-      }),
+      body: AccountCreateForm(
+        onSubmit: (AccountModel account) async {
+          await accountRepository.save(account);
+          Navigator.of(context).pop();
+        },
+      ),
     );
   }
 }
