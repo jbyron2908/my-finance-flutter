8df34ecd88b1ea3b6e1665e667a592a8d72a464c
Jonathan Byron <jonathan.byron2908@gmail.com>
2020-03-25 22:34:30 +0100

    Fix screen template

--- blueprint-templates/screen/__snakeCase_name__/screen/__snakeCase_name___route.dart
+++ blueprint-templates/screen/__snakeCase_name__/screen/__snakeCase_name___route.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:my_finance_flutter/ui/common/base/screen/base_route.dart';
 
-class {{pascalCase name}}Route extends BaseRoute {
-  {{pascalCase name}}RouteArgs routeArguments;
-
-  {{pascalCase name}}Route({this.routeArguments});
+class {{pascalCase name}}Route extends BaseRoute<{{pascalCase name}}ScreenArgs> {
+  {{pascalCase name}}Route({ {{pascalCase name}}ScreenArgs argument })
+      : super(argument: argument);
 
   @override
   String get routePath => "/routeName";
 
   @override
   MaterialPageRoute generateRoute(RouteSettings routeSettings) {
-    var arguments = routeSettings.arguments as {{pascalCase name}}RouteArgs;
-
     return MaterialPageRoute(
-      builder: (context) => {{pascalCase name}}Screen(arguments),
+      builder: (context) => {{pascalCase name}}Screen(),
       settings: routeSettings,
     );
   }
-
-  @override
-  Object get arguments => this.routeArguments;
-}
-
-class {{pascalCase name}}RouteArgs {
-  int field1;
-
-  {{pascalCase name}}RouteArgs({
-    this.field1,
-  });
 }
--- blueprint-templates/screen/__snakeCase_name__/screen/__snakeCase_name___screen.dart
+++ blueprint-templates/screen/__snakeCase_name__/screen/__snakeCase_name___screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_finance_flutter/ui/common/base/screen/base_screen.dart';
+import 'package:provider/provider.dart';
 
-class {{pascalCase name}}Screen extends BaseScreen<{{pascalCase name}}Bloc> {
-  {{pascalCase name}}Screen(this.screenArgs);
-
-  final {{pascalCase name}}RouteArgs screenArgs;
-
+class {{pascalCase name}}Screen extends BaseScreen<{{pascalCase name}}Bloc, {{pascalCase name}}ScreenArgs> {
   @override
   Widget build(BuildContext context) {
     return {{pascalCase name}}View();
@@ -17,4 +14,15 @@ class {{pascalCase name}}Screen extends BaseScreen<{{pascalCase name}}Bloc> {
       context: context,
     );
   }
+}
+
+class {{pascalCase name}}ScreenArgs {
+  static {{pascalCase name}}ScreenArgs of(BuildContext context) =>
+    Provider.of<{{pascalCase name}}ScreenArgs>(context, listen: false);
+
+  int field1;
+
+  {{pascalCase name}}ScreenArgs({
+    this.field1,
+  });
 }
