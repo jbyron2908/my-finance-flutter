1ad37b9b2b60f9eb2287c98609ab08fa59d5f0dc
Jonathan Byron <jonathan.byron2908@gmail.com>
2020-05-06 14:54:40 +0200

    Delete old test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'category/list_mock.dart';
-
 void main() {
   testWidgets('Category list test', (WidgetTester tester) async {
     // await tester.pumpWidget(CategoryListMock());
     await tester.pumpWidget(
       MaterialApp(
         title: 'Flutter Demo',
-        home: CategoryListMock(),
       ),
     );
 
