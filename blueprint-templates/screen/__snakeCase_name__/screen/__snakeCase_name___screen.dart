542e15ee95a7b3ba4d7662ba1cd197337f120d94
Jonathan Byron <jonathan.byron2908@gmail.com>
2021-01-26 16:47:31 +0100

    Fix templates

--- blueprint-templates/screen/__snakeCase_name__/screen/__snakeCase_name___screen.dart
+++ blueprint-templates/screen/__snakeCase_name__/screen/__snakeCase_name___screen.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:my_finance_flutter/ui/common/base/screen/base_screen.dart';
+import 'package:get/get.dart';
+import 'package:my_finance_flutter/ui/common/navigation/navigation_handler.dart';
+import 'package:my_finance_flutter/ui/screen/main/widget/main_tab_router.dart';
 
-class {{pascalCase name}}Screen extends BaseScreen<{{pascalCase name}}Bloc, {{pascalCase name}}RouteArgs> {
+class {{pascalCase name}}Screen extends StatelessWidget {
+  static String get _routeName => '/newScreen';
+  static RouteDefinition get routeDefinition => RouteDefinition(
+        name: _routeName,
+        routeBuilder: (routeSetting) => GetPageRoute(
+          settings: routeSetting,
+          page: () => {{pascalCase name}}Screen(),
+          binding: {{pascalCase name}}Binding(),
+        ),
+      );
+
+  static void navigateTo() {
+    MainTabRouter.navigateTo(_routeName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return {{pascalCase name}}View();
   }
-
-  @override
-  {{pascalCase name}}Bloc buildBloc(BuildContext context) {
-    return {{pascalCase name}}Bloc(
-      context: context,
-    );
-  }
 }
 
+class {{pascalCase name}}Binding implements Bindings {
+  @override
+  void dependencies() {
+    Get.put({{pascalCase name}}Controller());
+  }
+}
